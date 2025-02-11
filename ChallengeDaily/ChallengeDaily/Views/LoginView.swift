1deaf1f9852f3703387260f65f516f1b2868a7ed
HeresJohnny1722 <jonathan.krolak@icloud.com>
2025-02-11 11:00:29 -0500

    new Views

---
+++ ChallengeDaily/ChallengeDaily/Views/LoginView.swift
@@ -0,0 +1,107 @@
+//
+//  LoginView.swift
+//  ChallengeDaily
+//
+//  Created by Jonathan on 2/4/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    var body: some View {
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack {
+                    Text("Welcome Back!")
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    Spacer()
+                }
+                .padding()
+                .padding(.top)
+                
+                Spacer()
+                
+                HStack{
+                    Image(systemName: "mail")
+                    TextField("Email", text: $email)
+                    
+                    Spacer()
+                    
+                    if (email.count != 0) {
+                        Image(systemName: "checkmark")
+                            .fontWeight(.bold)
+                            .foregroundColor(.green)
+                    }
+                    
+                }
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.black)
+                }
+                
+                .padding()
+                
+                HStack{
+                    Image(systemName: "lock")
+                    TextField("Password", text: $password)
+                    
+                    Spacer()
+                    
+                    if (password.count != 0) {
+                        Image(systemName: "checkmark")
+                            .fontWeight(.bold)
+                            .foregroundColor(.green)
+                    }
+                    
+                }
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.black)
+                }
+                
+                .padding()
+                
+                Button(action: {}) {
+                    Text("Don't have an account?")
+                        .foregroundColor(.black.opacity((0.7)))
+                }
+                
+                Spacer()
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign In")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .bold()
+                    
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                    
+                        .background{
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.black)
+                        }
+                        .padding(.horizontal)
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
