21c60a9e754ff790b961e74a064f07567e534db0
HeresJohnny1722 <jonathan.krolak@icloud.com>
2025-02-21 14:24:27 -0500

    asdfsdfs

--- ChallengeDaily/ChallengeDaily/Views/MainView.swift
+++ ChallengeDaily/ChallengeDaily/Views/MainView.swift
@@ -9,40 +9,48 @@ struct MainView: View {
             VStack {
                 Spacer().frame(height: 10)
 
-                ZStack {
-                    HStack {
-                        Button(action: {
-                            showSocial = true // Navigate to SocialView
-                        }) {
-                            Image(systemName: "person.2.fill")
-                                .resizable()
-                                .frame(width: 35, height: 25)
-                                .foregroundColor(.white)
-                        }
-                        Spacer()
+                HStack {
+                    Button(action: {
+                        showSocial = true // Navigate to SocialView
+                    }) {
+                        Image(systemName: "person.2.fill")
+                            .resizable()
+                            .frame(width: 35, height: 25)
+                            .foregroundColor(.white)
                     }
-                    .padding(.horizontal)
-
+                    Spacer()
                     Text("Today's Challenge")
                         .font(.title2)
                         .foregroundColor(.white)
                         .fontWeight(.black)
-
-                    HStack {
-                        Spacer()
-
-                        Button(action: {
-                            showProfile = true // Navigate to ProfileView
-                        }) {
-                            Image(systemName: "person.crop.circle.fill")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(.white)
-                        }
+                    Spacer()
+                    Button(action: {
+                        showProfile = true // Navigate to ProfileView
+                    }) {
+                        Image(systemName: "person.crop.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white)
                     }
-                    .padding(.horizontal)
                 }
-                
+                .padding(.horizontal)
+
+                Spacer().frame(height: 20) // Adjust spacing
+
+                VStack {
+                    Text("Daily Challenge")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.white)
+                )
+
                 Spacer()
             }
             .navigationDestination(isPresented: $showProfile) {
